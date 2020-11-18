6ce2aeb4c99ffcecb410fe48e703c2515d92d435
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-18 15:25:48 -0700

    Adds components to return and sets up state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ImageContainer from '../components/ImageContainer'
+import ImageModel from '../models/images';
 
 const Home = () => {
+  const [images,setImages] = useState([])
+
+  const fetchData=() => {
+    ImageModel.all().then(data=>{
+      console.log(data)
+    })
+  }
+
+  useEffect(()=>{fetchData()})
+
   return (
     <div>
       <h1>Welcome all Pet Owners</h1>
+      <ImageContainer />
     </div>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import ImageContainer from '../components/ImageContainer'
 
 const Profile = () => {
+    // One ImageContainer for pets and one for all photots
     return (
+        <div>
+        <ImageContainer /> 
+        <ImageContainer />
         <h1>Profile</h1>
+        </div>
     )
 }
 
