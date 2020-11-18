4599df2e7365bca3c5ffe50a281a8d01d4a48ec0
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-18 16:01:15 -0700

    Get data from backend and serve it to frontend

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -4,10 +4,13 @@ import React from 'react';
 const ImageContainer = (props) => {
     // If we are on home page display up to 8 img 
     // If on profile page half the size and 2 comontents  
- 
+    const images = props.images.map(image =>(
+        <img className={props.class} src={image.imageUrl} alt="Users personal photos" />
+    ))
+    
     return (
         <div>
-            <img src={props.imgUrl} alt=""/>
+            {images}
         </div>
     );
 }
