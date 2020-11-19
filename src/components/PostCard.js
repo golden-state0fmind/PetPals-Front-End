7a40ce2d43d9999b1dae07d658bd09b4eaeb792d
smashflashtech <smash.flash.tech@gmail.com>
2020-11-19 14:50:56 -0800

    adds ternary statement to show edit post button for posts associated with current user

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const PostCard = (props) => {
-    const posts = props.posts.map((post, index)=>(
+    const userId = localStorage.getItem('id')
+    console.log ("this is userid", userId)
+    const posts = props.posts.map((post, index) => (
         <div>
-        <h3>{post.userId}</h3> 
-        <p> {post.content} </p>
-        <img src={post.imgUrl} alt=""/> 
-        <p>{post.createAt}</p>
-        <button> <Link to={'/addcomment'}>Add Comment</Link> </button>
-      </div>
+            <h3>{post.userId}</h3>
+            <p> {post.content} </p>
+            <img src={post.imgUrl} alt="" />
+            <p>{post.createAt}</p>
+            <button><Link to={'/addcomment'}>Add Comment</Link></button>
+            {post.userId == userId ? 
+
+            <button><Link to={'/editpost'}>Edit Post</Link></button> : "" }
+        </div>
     ))
     return (
         <>
-        {posts}
+            {posts}
         </>
     );
 }
