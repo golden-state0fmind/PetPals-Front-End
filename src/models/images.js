043ee622369e0895ad275a291a05ba519a03a230
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-18 17:04:37 -0700

    Deletes unused code

--- src/models/images.js
+++ src/models/images.js
@@ -5,20 +5,7 @@ class ImageModel {
     return fetch(`${url}/images`).then(res => res.json())
   }
 
-//   static show = (gameId) => {
-//     return fetch(`${url}/games/${gameId}`).then(res => res.json())
-//   }
 
-//   static create = (gameData) => {
-//     return fetch(`${url}/games`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify(gameData)
-//     })
-//       .then(res => res.json())
-//   }
 }
 
 export default ImageModel
