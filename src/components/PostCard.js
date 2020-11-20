945b6632c0a8b1e953fbe0274199957fa997d791
smashflashtech <smash.flash.tech@gmail.com>
2020-11-19 16:48:58 -0800

    passes post id through link url to edit post

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 const PostCard = (props) => {
+    console.log ("this is props", props.posts)
     const userId = localStorage.getItem('id')
-    console.log ("this is userid", userId)
     const posts = props.posts.map((post, index) => (
         <div>
-            <h3>{post.userId}</h3>
-            <p> {post.content} </p>
+            <h3>USER ID: {post.userId}</h3>
+            <h3>POST ID: {post.id}</h3>
             <img src={post.imgUrl} alt="" />
+            <p> {post.content} </p>
             <p>{post.createAt}</p>
             <button><Link to={'/addcomment'}>Add Comment</Link></button>
             {post.userId == userId ? 
 
-            <button><Link to={'/editpost'}>Edit Post</Link></button> : "" }
+            <button><Link to={`/post/${post.id}/edit`}>Edit Post</Link></button> : "" }
         </div>
     ))
     return (
