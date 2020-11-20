fe923cdee19f13da81a72592fbae9af1f2b73a92
smashflashtech <smash.flash.tech@gmail.com>
2020-11-19 16:54:19 -0800

    adds edit post functionality, still needs delete functionality

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PostModel from '../models/post'
 
-const EditPost = () => {
-    return (
-        <div>
-            EditPost
-        </div>
-    )
-}
+const EditPost = (props) => {
+    const [postId, setPostId] = useState(props.match.params.id) 
+    const [content, setContent] = useState('') 
+    const [imgUrl, setImgUrl] = useState('')
+    const [userId, setUser] = useState(localStorage.getItem('id'))
 
-export default EditPost
+    const fetchPost = () => {
+        PostModel.onePost(postId).then((postData) => {
+            setContent(postData.post.content)
+            setImgUrl(postData.post.imgUrl)
+            setUser(postData.post.userId)
+        })
+    }
+
+    useEffect(() => { fetchPost() }, [])
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        PostModel.update({
+            content,
+            userId,
+        }, postId).then(data => {
+            props.history.push('/')
+        })
+    }
+
+    //need a function for the delete button 
+
+        return (
+            <div>
+                <img src={imgUrl} alt="This is your post image"/>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <textarea
+                            onChange={(e) => { setContent(e.target.value) }}
+                            rows="5"
+                            cols="40"
+                            type="text"
+                            id="content"
+                            name="content"
+                            value={content}
+                        />
+                    </div>
+                    <button type="submit">Save Changes</button>
+                    <button type="submit">Delete Post</button>
+                </form>
+            </div>
+        )
+    }
+
+    export default EditPost
