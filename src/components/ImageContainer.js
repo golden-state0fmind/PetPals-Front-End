b9fdaa8a4db06265d250f882c1d7f4f10fd812c9
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-23 01:45:49 -0700

    Moves ImageContainer into two smaller componenets

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,21 +1,19 @@
 import React, {useState} from 'react';
+import Image from './Image'
 
 
 const ImageContainer = (props) => {
     const [currentUserId, setcurrentUserId] = useState(localStorage.getItem('id'))
     // If we are on home page display up to 8 img 
     // If on profile page half the size and 2 comontents  
+    console.log(props.imgClass)
     const images = props.images.map((image, index) => (
-        <div key={index}>
-        { image.userId == currentUserId ?
-        <img className={props.imgClass} src={image.imageUrl} alt="Users personal photos"  />
-        : ""}
-        </div>
+        <Image key={index} imgUrl={image.imageUrl} imgClass={props.imgClass}/>
     ))
 
     return (
         <div className={props.divClass}>
-            {images}
+            {images}1
         </div>
     );
 }
