eb63cf494beaeec834425550f7e31fce0b9a146b
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-23 21:09:41 -0700

    starts styling of the navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,62 @@
-import React, {useState, useEffect} from 'react'
-import Images from '../components/Images'
-import PostBar from '../components/PostBar'
-import PostContainer from '../components/PostContainer'
-import {Link} from 'react-router-dom'
-import '../css/home.css'
-import PostModel from '../models/post'
-import RelationshipModel from '../models/relationship'
+import React, { useState, useEffect } from "react";
+import Images from "../components/Images";
+import PostBar from "../components/PostBar";
+import PostContainer from "../components/PostContainer";
+import { Link } from "react-router-dom";
+import "../css/home.css";
+import PostModel from "../models/post";
+import RelationshipModel from "../models/relationship";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Home = (props) => {
-  const [images,setImages] = useState([])
-  const [posts, setPosts] = useState([])
-  const [userId] = useState(localStorage.getItem('id'))
-  
-  const fetchUser = () =>{
-    RelationshipModel.one(userId).then(user =>{
-      setImages(user.user.images)
-      console.log(user.user)
-    })
-  }
-  
-  const fetchPosts = () =>{
-    PostModel.all().then((postData)=>{
-      setPosts(postData.posts)
-    })
-  }
+  const [images, setImages] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [userId] = useState(localStorage.getItem("id"));
 
+  const fetchUser = () => {
+    RelationshipModel.one(userId).then((user) => {
+      setImages(user.user.images);
+      console.log(user.user);
+    });
+  };
+
+  const fetchPosts = () => {
+    PostModel.all().then((postData) => {
+      setPosts(postData.posts);
+    });
+  };
 
   // useEffect( () => { fetchImages() },[])
-  useEffect( () => { fetchPosts() } ,[])
-  useEffect( () => { fetchUser() } ,[])
-
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
-    <div>
+    <Container>
       <h1>Welcome all Pet Owners</h1>
-      <Images images={images} imgClass="home-preview-img" divClass="home-preview-container"/>
-      <Link to={'/uploadphotos'}><button>Upload Photos</button></Link>
-      <PostBar />
-      {posts.length ?  <PostContainer posts={posts}/> : "Loading!"}
-    
-    </div>
+      <Row>
+        <Col>
+          <Images
+            images={images}
+            imgClass="home-preview-img"
+            divClass="home-preview-container"
+          />
+          <Link to={"/uploadphotos"}>
+            <button>Upload Photos</button>
+          </Link>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <PostBar />
+          {posts.length ? <PostContainer posts={posts} /> : "Loading!"}
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default Home;
