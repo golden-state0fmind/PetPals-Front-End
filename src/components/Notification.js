2e6f5c84a4e371c1ff46cae9ea3bf5b14bcbcb82
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-26 02:21:49 -0700

    Gets all pending user data from backend and displays on notification screen

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Notification = () => {
+const Notification = (props) => {
+    console.log(props.user)
     return (
         <div>
-        
+            <h3>{props.user.firstName} {props.user.lastName} </h3>
+            <button>Add Friend</button>
         </div>
     );
 }
--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Notification from '../components/Notification'
+import RelationshipModel from '../models/relationship'
 
 const Notifications = () => {
+    
+    const [userId] = useState(localStorage.getItem('id'))
+    const [users, setUsers] = useState([])
+
+   const fethData = () => {
+       RelationshipModel.pending(userId).then( requests =>{
+            console.log(requests)
+            // Filtering out current user
+            for(let i=0; i<requests.relationships.length; i++){
+                if(requests.relationships[i].userOne.id !== parseInt(userId)){
+                    setUsers((oldArray) => [...oldArray, requests.relationships[i].userOne]);
+                }
+                if(requests.relationships[i].userTwo.id !== parseInt(userId)){  
+                    setUsers((oldArray) => [...oldArray, requests.relationships[i].userTwo]);
+                }
+            }
+       })
+    }
+    console.log(users)
+
+    const request = users.map((user, index) =>(
+        <Notification user={user} key={index}/>
+    ))
+   
+
+    useEffect(()=>{fethData()},[])
     return (
         <div>
-            <Notification />
+            {request}
         </div>
     );
 }
