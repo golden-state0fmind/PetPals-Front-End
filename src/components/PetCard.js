eca036a6f4a11c643bf0dbe49f6c5315475331d0
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-25 17:08:14 -0700

    Adds a condtion check if the user doesn't have pets to not display

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -11,24 +11,17 @@ const PetCard = (props) => {
  
   console.log(props.pet)
   return (
+     <>
+    {props.pet ? 
     <div className="pet-card">
       <img className="pet-card-img" src={props.pet.imgUrl} alt=""/>
       <h4>{props.pet.name}</h4>
       <p>{props.pet.birthdate}</p>
     </div>
+    : "dothis"}
+ 
+    </>
 
-
-  //   <Card style={{ width: '18rem' }}>
-  //   <Card.Img variant="top" src={props.pet} />
-  //   <Card.Body>
-  //     <Card.Title>Card Title</Card.Title>
-  //     <Card.Text>
-  //       Some quick example text to build on the card title and make up the bulk of
-  //       the card's content.
-  //     </Card.Text>
-  //     <Button variant="primary">Go somewhere</Button>
-  //   </Card.Body>
-  // </Card>
   )
 }
 
