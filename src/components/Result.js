7cafd17f7d47fa603958bba1c6c3ad20f0b4b47b
dannymckinney88 <dannymckinney88@gmail.com>
2020-11-27 17:39:29 -0700

    Adds a conditional to check for current user

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 const Result = (props) => {
+  const [userId] = useState(parseInt(localStorage.getItem('id')))
     return (
         <div>
-        <Link to={`/user/${props.user.id}/profile`}><h3> {props.user.firstName} {props.user.lastName}</h3></Link> 
+        {userId === props.user.id ?
+        <Link to={`/user/${props.user.id}/profile`}><h3> {props.user.firstName} {props.user.lastName}</h3></Link> :
+
+        <Link to={`/profile`}><h3> {props.user.firstName} {props.user.lastName}</h3></Link>
+        }
       </div>
     )
 }
