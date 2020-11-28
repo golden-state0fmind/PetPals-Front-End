92ad3fe70f5a4e6748f6e0cc3ccd259cbd381952
smashflashtech <smash.flash.tech@gmail.com>
2020-11-28 15:32:26 -0800

    adds if statement to prevent creation of arrays with undefined elements, adds for loop to create image display array

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Images from "../components/Images";
 import PostBar from "../components/PostBar";
 import PostContainer from "../components/PostContainer";
-// import "../css/profile.css";
+import "../css/profile.css";
 import RelationshipModel from "../models/relationship";
 import ImageModel from "../models/images";
 import PetCardContainer from "../components/PetCardContainer";
@@ -26,7 +26,6 @@ const Profile = () => {
   const fetchUser = () => {
     RelationshipModel.one(userId).then((user) => {
       setPosts(user.user.posts)
-      setImages(user.user.images)
       setProfilePic(user.user.imgUrl)
       setFirstName(user.user.firstName)
       setLastName(user.user.lastName)
@@ -34,60 +33,72 @@ const Profile = () => {
 
       setPosts(user.user.posts);
       setUserName(`${user.user.firstName} ${user.user.lastName}`);
-      for (let i = 0; i < 4; i++) {
-        if (user.user.pets.length >= 1) {
-          setPets((oldArray) => [...oldArray, user.user.pets[i]]);
+
+      if (user.user.pets.length > 4) {
+        for (let i = 0; i < 4; i++) {
+          if (user.user.pets.length >= 1) {
+            setPets((oldArray) => [...oldArray, user.user.pets[i]]);
+          }
         }
-      }
-      console.log(user.user, userName);
-    });
-  };
+      } else { setPets(user.user.pets) }
+
+      if (user.user.images.length > 4) {
+        for (let i = 0; i < 4; i++) {
+          if (user.user.images.length >= 1) {
+            setImages((oldArray) => [...oldArray, user.user.images[i]]);
+          }
+        }
+      } else { setImages(user.user.images) }
+
+    })
+  }
 
 
-  useEffect(() => {
-    fetchUser();
-  }, []);
 
-  return (
+
+
+useEffect(() => {
+  fetchUser();
+}, []);
+
+return (
+  <>
+    <div className="nameDisplay"><h1>{firstName} {lastName}</h1></div>
+    <div className="profilePicContainer">
+      {profilePic !== "" ?
+        <img className="profilePic" src={profilePic} alt="Profile Picture" />
+        : <div className="addProfilePic"><Link className="addProfilePicText" to="/allphotos">Add a Profile Pic from your Photos</Link></div>}
+    </div>
+    <PostBar />
+
     <Container fluid>
-      <div className="nameDisplay"><h1>{firstName} {lastName}</h1></div>
-          <div className="profilePicContainer">
-            {profilePic !== "" ?
-              <img className="profilePic" src={profilePic} alt="Profile Picture" />
-              : <div className="addProfilePic"><Link className="addProfilePicText" to="/allphotos">Add a Profile Pic from your Photos</Link></div>}
-          </div>
-          <PostBar />
-
-
-
       <Row className="img-wrapper">
         <Col sm={4} className="img-container">
-
           <Images
             imgClass="profile-preview-img"
             divClass="profile-preview-container"
             images={images}
           />
-          <Link to="/allphotos">See All</Link>{" "}
+          <Link to="/allphotos">See All</Link>
           <Link to={"/uploadphotos"}>Upload Photos</Link>
         </Col>
 
         <Col sm={4} className="profile-header">
           <PetCardContainer pets={pets} />
           <div className="pet-card-btns-container">
-          {/* <Images imgClass="profile-preview-img" divClass="profile-preview-container" images={pets}/> */}
+            {/* <Images imgClass="profile-preview-img" divClass="profile-preview-container" images={pets}/> */}
             <Link className="pets-btn" to="/allpets">
-              {" "}
-              See All Pets{" "}
-            </Link>{" "}
+
+              See All Pets
+              </Link>
             <Link className="pets-btn" to="/addpet">
-              {" "}
-              Add A Pet{" "}
-            </Link>{" "}
+
+              Add A Pet
+              </Link>
           </div>
         </Col>
       </Row>
-      
+
       <Row>
         <Col className="friends-container">
           <Friends />
@@ -102,7 +113,8 @@ const Profile = () => {
         )}
 
     </Container>
-  );
+  </>
+);
 };
 
 export default Profile;
