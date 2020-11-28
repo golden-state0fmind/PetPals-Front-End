d246c3fd7fb26a0c1a64d354374912c853a28d7e
smashflashtech <smash.flash.tech@gmail.com>
2020-11-28 13:10:53 -0800

    removes console log and adds add a pet link to ternary

--- src/components/PetCardContainer.js
+++ src/components/PetCardContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PetCard from './PetCard'
+import {Link} from 'react-router-dom'
 
 const PetCardContainer = (props) => {
     const pets = props.pets.map((pet,index) =>(
@@ -7,7 +8,7 @@ const PetCardContainer = (props) => {
     ))
     return (
         <div className="pet-container">
-            {pets.length>= 1 ? pets: <p>No pets added</p>}
+            {pets.length>= 1 ? pets: <Link to="/addpet">Add A Pet</Link>}
         </div>
     );
 }
