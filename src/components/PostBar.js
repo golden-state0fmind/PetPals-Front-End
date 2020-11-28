0eae82ffd1fe12109c8c5860a785bc9b495e1542
smashflashtech <smash.flash.tech@gmail.com>
2020-11-28 13:01:28 -0800

    styles post bar

--- src/components/PostBar.js
+++ src/components/PostBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import '../css/postBar.css'
 
 const PostBar = () => {
     return (
-        <div>
-            <button> <Link to={'/createpost'}>Create a post</Link> </button>
+        <div className="align-statusbar"> 
+        <div className="fake-statusbar-container">
+        <div className="fake-statusbar"><Link className="fake-statustext" to={'/createpost'}>Say something...</Link></div>
+            {/* <button className="pb-button"> <Link className="pb-button" to={'/createpost'}>Create a post</Link> </button> */}
+        </div>
         </div>
     );
 }
