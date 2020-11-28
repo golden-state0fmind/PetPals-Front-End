337d65e386142630315a2269c2441ae0620e193a
smashflashtech <smash.flash.tech@gmail.com>
2020-11-28 11:33:15 -0800

    re-adds edit pet button and ternary to only display edit button for pets that belong to user

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -14,8 +14,10 @@ const PetCard = (props) => {
       <img className="pet-card-img" src={props.pet.imgUrl} alt=""/>
       <h4>{props.pet.name}</h4>
       <p>{props.pet.birthdate}</p>
+      {/* {userId === props.pet.userId ? <button><Link to={`/pet/${props.pet.id}/edit`}>Edit Pet</Link></button> : ""} */}
+      <button><Link to={`/pet/${props.pet.id}/edit`}>Edit Pet</Link></button>
     </div>
-    : <p></p>}
+    : ""}
  
     </>
 
