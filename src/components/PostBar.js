531a70aa2073de5cf1c8660dbce14aea99e3792e
smashflashtech <smash.flash.tech@gmail.com>
2020-11-28 13:09:19 -0800

    styles the post bar

--- src/components/PostBar.js
+++ src/components/PostBar.js
@@ -4,11 +4,8 @@ import '../css/postBar.css'
 
 const PostBar = () => {
     return (
-        <div className="align-statusbar"> 
         <div className="fake-statusbar-container">
         <div className="fake-statusbar"><Link className="fake-statustext" to={'/createpost'}>Say something...</Link></div>
-            {/* <button className="pb-button"> <Link className="pb-button" to={'/createpost'}>Create a post</Link> </button> */}
-        </div>
         </div>
     );
 }
