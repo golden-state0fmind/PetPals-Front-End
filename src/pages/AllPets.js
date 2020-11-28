3be46b5b7085d08c418a7807287b8f58dedc7d83
smashflashtech <smash.flash.tech@gmail.com>
2020-11-28 11:30:58 -0800

    adds add a pet button

--- src/pages/AllPets.js
+++ src/pages/AllPets.js
@@ -1,27 +1,34 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import PetCard from '../components/PetCard'
-import PetModel from '../models/pet'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import RelationshipModel from "../models/relationship";
 import PetCardContainer from '../components/PetCardContainer'
+
 
 const AllPets = (props) => {
     const [pets, setPets] = useState([])
+    const [userId] = useState(localStorage.getItem('id'))
 
 
-    const fetchPets = () => {
-        PetModel.all().then((petData) => {
-            setPets(petData.pets)
+    // const fetchPets = () => {
+    //     PetModel.all().then((petData) => {
+    //         setPets(petData.pets)
+    //     })
+    // }
+
+    const fetchUserPets = () => {
+        RelationshipModel.one(userId).then(user => {
+            setPets(user.user.pets)
         })
     }
 
-    useEffect(()=> {fetchPets()},[])
+    useEffect(() => { fetchUserPets() }, [])
 
 
     console.log(pets)
     return (
         <div>
             <h1>All Pets Page</h1>
-            {/* <PetCard pets={pets}/> */}
+            <button> <Link to="/addpet">Add A Pet</Link> </button>
             <PetCardContainer pets={pets} />
         </div>
     )
