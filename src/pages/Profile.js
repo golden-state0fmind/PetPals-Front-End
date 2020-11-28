12c1acf3b317d00bda44b9f4f52e260b480c8f73
smashflashtech <smash.flash.tech@gmail.com>
2020-11-27 16:38:55 -0800

    noodling with ternary for profile pic display, not working

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,17 +7,17 @@ import '../css/profile.css'
 import RelationshipModel from '../models/relationship'
 
 const Profile = () => {
-    const [userId] = useState(localStorage.getItem('id'))
-    const [profilePic, setProfilePic] = useState('')
-    const [posts, setPosts] = useState([])
-    const [images, setImages] = useState([])
-    const [pets, setPets] = useState([])
-    const [firstName, setFirstName] = useState()
-    const [lastName, setLastName] = useState()
-    const [birthdate, setBirthDate] = useState()
+  const [userId] = useState(localStorage.getItem('id'))
+  const [profilePic, setProfilePic] = useState('')
+  const [posts, setPosts] = useState([])
+  const [images, setImages] = useState([])
+  const [pets, setPets] = useState([])
+  const [firstName, setFirstName] = useState()
+  const [lastName, setLastName] = useState()
+  const [birthdate, setBirthDate] = useState()
 
-  const fetchUser = () =>{
-    RelationshipModel.one(userId).then(user =>{
+  const fetchUser = () => {
+    RelationshipModel.one(userId).then(user => {
       setPosts(user.user.posts)
       setImages(user.user.images)
       setPets(user.user.pets)
@@ -29,26 +29,30 @@ const Profile = () => {
     })
   }
 
-    useEffect(() => { fetchUser() }, [])
-    return (
-        <div>
-            <h1>{firstName} {lastName}</h1>
-            <div className="row"></div>
-              <div className="col p-left"><img src={profilePic} alt="Profile Picture"/></div> 
-              <div className="col p-right">
-                <ul>
-                  <li><button><Link to="/allphotos">See All Photos</Link></button></li>
-                  <li><button><Link to="/allpets">See All Pets</Link></button></li>
-                  <li><button><Link to="/addpet">Add A Pet</Link></button></li>
-                  <li><button><Link to={'/uploadphotos'}>Upload Photos</Link></button></li>
-                </ul>
-              </div>
-              <Images imgClass="profile-preview-img" divClass='profile-preview-container' images={pets} />
-            <Images imgClass="profile-preview-img" divClass='profile-preview-container' images={images} />
-            <PostBar />
-            {posts.length ? <PostContainer posts={posts} /> : "Loading!"}
-        </div>
-    )
+  useEffect(() => { fetchUser() }, [])
+  return (
+    <div className="Profile">
+      <div className="nameDisplay"><h1>{firstName} {lastName}</h1></div>
+          <div className="profilePicContainer">
+            {profilePic !== "" ?
+              <img className="profilePic" src={profilePic} alt="Profile Picture" />
+              : <><div className="addProfilePic"><Link className="addProfilePicText" to="/allphotos">Add a Profile Pic from your Photos</Link></div></>}
+          </div>
+        <div className="buttons">
+          <ul>
+            <li><button><Link to="/allphotos">See All Photos</Link></button></li>
+            <li><button><Link to="/allpets">See All Pets</Link></button></li>
+            <li><button><Link to="/addpet">Add A Pet</Link></button></li>
+            <li><button><Link to={'/uploadphotos'}>Upload Photos</Link></button></li>
+          </ul>
+        
+      </div>
+      <Images imgClass="profile-preview-img" divClass='profile-preview-container' images={pets} />
+      <Images imgClass="profile-preview-img" divClass='profile-preview-container' images={images} />
+      <PostBar />
+      {posts.length ? <PostContainer posts={posts} /> : "Loading!"}
+    </div>
+  )
 }
 
 export default Profile
