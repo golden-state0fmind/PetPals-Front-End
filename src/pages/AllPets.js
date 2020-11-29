2ce6569ac37d7444ffd6456aeba0a36475f463ca
smashflashtech <smash.flash.tech@gmail.com>
2020-11-28 21:30:02 -0800

    centers header and styles button

--- src/pages/AllPets.js
+++ src/pages/AllPets.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import RelationshipModel from "../models/relationship";
 import PetCardContainer from '../components/PetCardContainer'
-
+import '../css/allPets.css'
 
 const AllPets = (props) => {
     const [pets, setPets] = useState([])
@@ -19,9 +19,9 @@ const AllPets = (props) => {
 
     console.log(pets)
     return (
-        <div>
-            <h1>All Pets Page</h1>
-            <button> <Link to="/addpet">Add A Pet</Link> </button>
+        <div className="allp-body">
+            <h1 className="allp-heading">My Pets</h1>
+            <button className="allp-button"> <Link className="allp-link" to="/addpet">Add Pet</Link></button>
             <PetCardContainer pets={pets} />
         </div>
     )
