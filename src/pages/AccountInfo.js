0378af6067d62f75e7f6cbed0a983c6167fb711c
smashflashtech <smash.flash.tech@gmail.com>
2020-11-29 04:16:39 -0800

    adds styles to account info

--- src/pages/AccountInfo.js
+++ src/pages/AccountInfo.js
@@ -21,12 +21,14 @@ const AccountInfo = () => {
     useEffect(() => { fetchUser() }, [])
 
     return (
-        <div>
-            <h1 className="ai-heading">Account Info</h1>
-            <p><span class="bold">Name:</span> {firstName} {lastName}</p>
-            <p><span class="bold">Birthday:</span> {birthdate}</p>
-            <p><span class="bold">Email:</span> {email}</p>
-            {/* <button><Link to={`/post/${userId}/edit`}>Edit Info</Link></button> */}
+        <div className="ai-body">
+            <div className="ai-content">
+                <h1 className="ai-heading">Account Info</h1>
+                <p className="ai-detail"><span className="bold">Name:</span> {firstName} {lastName}</p>
+                <p className="ai-detail"><span className="bold">Birthday:</span> {birthdate}</p>
+                <p className="ai-detail"><span className="bold">Email:</span> {email}</p>
+                {/* <button><Link to={`/post/${userId}/edit`}>Edit Info</Link></button> */}
+            </div>
         </div>
     )
 }
