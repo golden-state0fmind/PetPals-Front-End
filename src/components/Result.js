607c36cc7bb7ab6f5b6a7548fcf1254ecca513be
smashflashtech <smash.flash.tech@gmail.com>
2020-11-29 22:35:13 -0800

    fixes ternary so that current user is directed to profile vs pals page when clicking on self

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,7 +5,7 @@ const Result = (props) => {
   const [userId] = useState(parseInt(localStorage.getItem('id')))
     return (
         <div>
-        {userId === props.user.id ?
+        {userId !== props.user.id ?
         <Link to={`/user/${props.user.id}/profile`}><h3> {props.user.firstName} {props.user.lastName}</h3></Link> :
 
         <Link to={`/profile`}><h3> {props.user.firstName} {props.user.lastName}</h3></Link>
