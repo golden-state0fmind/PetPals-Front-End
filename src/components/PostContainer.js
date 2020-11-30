922375f5b99fb019e4d8bed6664290c8fb41b53d
smashflashtech <smash.flash.tech@gmail.com>
2020-11-30 09:36:09 -0800

    adds posts heading

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PostCard from './PostCard'
+import '../css/profile.css'
 
 const PostContainer = (props) => {
 
     return (
-        <div className="post-wrapper">
+        <div className="posts-wrapper">
+        <div className="post-header"><h4 className="content-text">Posts</h4></div>
             <PostCard user={props.user} posts={props.posts}/>
         </div>
     );
